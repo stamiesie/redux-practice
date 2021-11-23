2a7023d26424323fd26575038f6677f986a26586
Stephen Tamiesie <stephen.tamiesie@gmail.com>
2021-11-23 15:17:18 -0800

    add log status button to isLogged conditional

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
+/* eslint-disable react/button-has-type */
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement, loggedStatus } from './actions';
 
 function App() {
   const counter = useSelector((state) => state.counter);
   const isLogged = useSelector((state) => state.isLogged);
+  const dispatch = useDispatch();
   return (
     <div className="App">
       <h1>Counter: {counter}</h1>
+      <button onClick={() => dispatch(increment())}>+ADD+</button>
+      <button onClick={() => dispatch(decrement())}>-MINUS-</button>
       {isLogged ? (
-        <h3>You are currently logged IN</h3>
+        <>
+          <button onClick={() => dispatch(loggedStatus())}>Logout</button>
+          <h3>You are currently logged IN</h3>
+        </>
       ) : (
-        <h3>You are currently logged OUT</h3>
+        <>
+          <button onClick={() => dispatch(loggedStatus())}>Login</button>
+          <h3>You are currently logged OUT</h3>
+        </>
       )}
     </div>
   );
---
+++ src/actions/index.js
@@ -0,0 +1,11 @@
+export const increment = () => ({
+  type: 'INCREMENT',
+});
+
+export const decrement = () => ({
+  type: 'DECREMENT',
+});
+
+export const loggedStatus = () => ({
+  type: 'SIGN_IN',
+});
