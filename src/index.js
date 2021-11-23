f965e5ae2de5cb2735c3f833229d29cef39f019f
Stephen Tamiesie <stephen.tamiesie@gmail.com>
2021-11-23 13:48:39 -0800

    counter state w/o react

--- src/index.js
+++ src/index.js
@@ -1,8 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { createStore } from 'redux';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+// STORE -> GLOBALIZED STATE
+
+// ACTION -> INCREMENT
+const increment = () => ({
+  type: 'INCREMENT',
+});
+
+const decrement = () => ({
+  type: 'DECREMENT',
+});
+
+// REDUCER
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+    default:
+  }
+};
+
+const store = createStore(counter);
+
+// Display in console
+store.subscribe(() => console.log(store.getState()));
+
+// DISPATCH
+store.dispatch(increment());
+store.dispatch(decrement());
 
 ReactDOM.render(
   <React.StrictMode>
