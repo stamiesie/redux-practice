fe50a1e96dc98517aef83bb6cbc6b719312e5d38
Stephen Tamiesie <stephen.tamiesie@gmail.com>
2021-11-23 14:54:27 -0800

    add useSelector to app.js, counter and login element with conditional

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+
 function App() {
-  return <div className="App">HELLO</div>;
+  const counter = useSelector((state) => state.counter);
+  const isLogged = useSelector((state) => state.isLogged);
+  return (
+    <div className="App">
+      <h1>Counter: {counter}</h1>
+      {isLogged ? (
+        <h3>You are currently logged IN</h3>
+      ) : (
+        <h3>You are currently logged OUT</h3>
+      )}
+    </div>
+  );
 }
 
 export default App;
